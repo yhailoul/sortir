57274d46c0ffb8f1249621003ad36551b2e7ac6b
yhailoul <yasmine.hailoul2025@campus-eni.fr>
2026-03-24 11:25:51 +0100

    ajout des fixtures (quelques détails à modifier pour ce qui concerne les relations entre les tables que je dois récupérer)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use phpDocumentor\Reflection\Location;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -24,10 +25,125 @@ class AppFixtures extends Fixture
                 ->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName())
                 ->setPhone($faker->phoneNumber())
-                ->setActive($faker->boolean())
-                ->setAdmin(false);
+                ->setActive($faker->boolean());
+            //mettre les attributs représentants liens entre les tables
+            //$manager->persist($user);
+            //$users[] = $user;
 
         }
-        $manager->flush();
+
+        $events=[];
+        $durations=[
+            new \DateInterval('PT30M'),
+            new \DateInterval('PT1H'),
+            new \DateInterval('PT1H30M'),
+            new \DateInterval('PT2H'),
+            new \DateInterval('PT2H30M'),
+            new \DateInterval('PT3H'),
+            new \DateInterval('PT3H30M'),
+            new \DateInterval('PT4H'),
+            new \DateInterval('P1D'),
+            new \DateInterval('P2D'),
+            new \DateInterval('P3D'),
+        ];
+        for ($i = 0; $i < 50; $i++) {
+            $event = new Event();
+            $event
+                ->setName($faker->sentence())
+                ->setinfosEvent($faker->paragraph())
+                ->setnbInscriptionMax($faker->numberBetween(2,15))
+                ->setDuration($faker->randomElement($durations))
+                ->setDateStartHour($faker->dateTimeBetween('now', '+30 day'))
+                ->setDateEndHour($faker->dateTimeBetween('+30 minutes', '+30 day'))
+                ->setDateLimitInscription($faker->dateTimeBetween('+2 day','+30 day'));
+            //mettre les attributs représentants liens entre les tables
+            //$manager->persist($event);
+        }
+        $campusList =[];
+        $campusEni = [
+            'Rennes',
+            'Quimper',
+            'Niort',
+            'Nantes',
+            'En-ligne'
+        ];
+        foreach ($campusEni as $campusName) {
+            $campus = new Campus();
+            $campusName
+                ->setName($campusName);
+            //ajouter la relation avec USER
+            //$manager->persist($campus);
+            $campusList[] = $campus;
+        }
+        $statusList =[];
+        $statuses=[
+            'En création',
+            'Ouverte',
+            'Clôturée',
+            'En cours',
+            'Terminée',
+            'Annulée',
+            'Historisée'
+        ];
+        foreach ($statuses as $statusName) {
+            $status = new Status;
+            $status->setName($statusName);
+            $statusList[] = $status;
+            //ajouter la relation avec la table EVENT
+            //$manager->persist($status);
+        }
+
+        $categoryNames = [
+            'Gastronomie et Détente ',
+            'Sport et Aventure ',
+            'Culture et découverte',
+            'Jeux et divertissement',
+            'Gaming',
+            'Bien-être et relaxation',
+            'Nature et Plein Air',
+            'Créatif et Atéliers',
+            'Nocturnes et Festivités',
+            'Insolite et Original'
+        ];
+
+        $categories = []; // Pour stocker les objets Category créés
+
+        foreach ($categoryNames as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $categories[] = $category;
+
+            //ajouter la relation avec la table EVENT
+            //$manager->persist($category);
+        }
+
+        for ($i = 0; $i < 50; $i++) {
+            $location = new Location();
+            $location
+                ->setName($faker->sentence())
+                ->setStreet($faker->streetAddress())
+                ->setLatitude($faker->latitude())
+                ->setLongitude($faker->longitude());
+            //mettre les attributs représentants liens entre les tables
+            //$manager->persist($location);
+        }
+
+
+        for ($i = 0; $i < 50; $i++) {
+            $city = new City();
+            $city
+                ->setName($faker->city())
+                ->setCodePostal($faker->postcode());
+            //mettre les attributs représentants liens entre les tables
+            //$manager->persist($location);
+        }
+
+
+
+
+
+
+        //$manager->flush();
     }
 }
