dd6f648b06239ed075758ba8620fe3976889d297
Camille <camillepayen35@gmail.com>
2026-03-24 14:52:15 +0100

    Ajout connexion par username ou email

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -12,7 +13,7 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 /**
  * @extends ServiceEntityRepository<User>
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
@@ -31,6 +32,15 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $user->setPassword($newHashedPassword);
         $this->getEntityManager()->persist($user);
         $this->getEntityManager()->flush();
+    }
+
+    public function loadUserByIdentifier($identifier): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :query OR u.email = :query')
+            ->setParameter('query', $identifier)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     //    /**
